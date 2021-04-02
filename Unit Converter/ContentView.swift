85b1b462eefcb2907164829d8ef5a89b3c6da01b
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-04-02 09:26:22 +0200

    Implement temp converting logic

--- Unit Converter/ContentView.swift
+++ Unit Converter/ContentView.swift
@@ -9,23 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var selectedInputUnit = TemperatureUnit.Celsuis
-    @State private var selectedOutputUnit = TemperatureUnit.Fahrenheit
-    @State private var userInput : String = ""
+    @State private var selectedInputTemperatureUnit = TemperatureUnit.Celsuis
+    @State private var selectedOutputTemperatureUnit = TemperatureUnit.Fahrenheit
+    @State private var userInputtedTemperature : String = "0"
+    
+    var inputTemperatureInCelsius: Double {
+        var temperatureInCelsius: Double
+        
+        switch selectedInputTemperatureUnit {
+        case .Celsuis:
+            temperatureInCelsius =  Double(userInputtedTemperature) ?? 0
+        case .Fahrenheit:
+            if let tempInFahrenheit = Double(userInputtedTemperature) {
+                temperatureInCelsius =   Double(tempInFahrenheit - 32) / 1.8
+            } else {
+                temperatureInCelsius = 0
+            }
+        case .Kelvin:
+            if let tempInKelvin =  Double(userInputtedTemperature) {
+                temperatureInCelsius = tempInKelvin - 273.15
+            } else {
+                temperatureInCelsius = 0
+            }
+        }
+        return temperatureInCelsius
+    }
+    
+    var convertedTemparature : Double {
+        let temperatureInCelsius = inputTemperatureInCelsius
+        var outputTemperature : Double
+        
+        switch selectedOutputTemperatureUnit {
+        case .Celsuis:
+            outputTemperature = temperatureInCelsius
+        case .Fahrenheit:
+            outputTemperature = (temperatureInCelsius * 1.8) + 32
+        case .Kelvin:
+            outputTemperature = (temperatureInCelsius + 273.15)
+        }
+        return outputTemperature
+    }
+    
     
     var body: some View {
         NavigationView {
             Form {
-                Section {
-                    TextField("Value to convert from", text: $userInput)
-                        .keyboardType(.decimalPad)
-                    TemperatureUnitPickerView(title: "Select input unit",selectedUnit:  $selectedInputUnit)
-                }.padding()
+                Section(header: Text("Unit you want to convert from.")) {
+                    TemperatureUnitPickerView(title: "Select input unit",selectedUnit:  $selectedInputTemperatureUnit)
+                }
                 
-                Section {
-                    TemperatureUnitPickerView(title: "Select output unit",selectedUnit:  $selectedOutputUnit)
-                    Text("\(userInput)")
-                }.padding()
+                TextField("Value to convert from", text: $userInputtedTemperature)
+                    .keyboardType(.decimalPad)
+                
+                Section(header: Text("Unit you want to convert to.")) {
+                    TemperatureUnitPickerView(title: "Select output unit",selectedUnit:  $selectedOutputTemperatureUnit)
+                }
+                
+                Text("\(convertedTemparature , specifier: "%.2f")")
             }
             .navigationBarTitle("Temperature Converter")
         }
