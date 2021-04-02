af0df92627d63a7f0ec18bad8481025e4a776a0c
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-04-02 07:32:11 +0200

    Initial Commit

---
+++ Unit Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unit Converter
+//
+//  Created by sanmi_personal on 02/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by sanmi_personal on 02/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
